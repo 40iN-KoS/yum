50a8f8414149fc2d3cb357d15670a484c942e29a
40iN_KoS <mr40@ya.ru>
2014-06-12 05:44:45 +0400

    dummy test classes

--- yum/tests.py
+++ yum/tests.py
@@ -1,1 +1,25 @@
 # -*- coding: utf-8 -*-
+
+import unittest
+
+# from yum import YummyDict
+
+
+class SimpleCheck(unittest.TestCase):
+    pass
+
+
+class ValidationCheck(unittest.TestCase):
+    pass
+
+
+class InterfaceCheck(unittest.TestCase):
+    pass
+
+
+class ReadWriteCheck(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
