bf2012dce570bd857c2c6c62e08b99e414ce3d9f
40iN_KoS <mr40@ya.ru>
2014-06-23 08:54:27 +0400

    renaming

--- yum/yum.py
+++ yum/yum.py
@@ -9,24 +9,29 @@ from errors import ValidationError as YumValidationError
 
 class YummyDict(object):
     _schema = {}  # TODO: schema validation
+    _new_keys = {}
+    _processing_mapping = {}
+
     __schema = {
         '$schema': 'http://json-schema.org/schema#',
         'type': 'object',
         'patternProperties': {'^[^_]': {}},
         'additionalProperties': False,
         }
-    _processing_mapping = {}
     __base_validator = Draft4Validator(__schema)
 
     def __init__(self, obj):
         self._validate(obj)
-        self._object = self._process(obj)
+        self._object = self._renaming(self._process(obj))
         self._raw_object = obj
 
     def __getattr__(self, name):
         if name.startswith('_'):
             raise AttributeError
-        return copy.deepcopy(self._object[name])
+        try:
+            return copy.deepcopy(self._object[name])
+        except KeyError:
+            raise AttributeError
 
     def __setattr__(self, name, value):
         if name not in ['_object', '_raw_object']:
@@ -60,4 +65,11 @@ class YummyDict(object):
         result = {}
         for k, v in obj.iteritems():
             result.update({k: cls._processing_mapping.get(k, lambda v: v)(v)})
+        return result
+
+    @classmethod
+    def _renaming(cls, obj):
+        result = {}
+        for k, v in obj.iteritems():
+            result.update({cls._new_keys.get(k, k): v})
         return result
